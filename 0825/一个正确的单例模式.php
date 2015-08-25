22961d1b4d348e2f99ecaf3e86baf891a1c84a67
GongBiao <developbiao@gmail.com>
2015-08-25 22:38:56 +0800

    学习jquery函数选择器

---
+++ 0825/一个正确的单例模式.php
@@ -0,0 +1,46 @@
+<?php
+/*
+面向对象复习
+*/
+header('Content-Type:text/html; charset=utf-8');
+class ShopCar{
+    private static $instance;
+    public $hash;
+    private function __construct(){
+        $this->hash = mt_rand(1, 99999);
+    } //私有化构造器
+    private function __clone(){} //私有化clone方法
+
+    public static function getCar(){
+       if(!(self::$instance instanceof self)){
+            self::$instance = new ShopCar();
+       } 
+
+       return self::$instance;
+    }
+
+
+
+}
+
+
+
+//$obj = new ShopCar();
+
+$obj = ShopCar::getCar(); 
+$obj2 = ShopCar::getCar();
+echo '<pre>';
+print_r($obj);
+echo '</pre>';
+echo '<h3>--------------</h3>';
+echo $obj->hash,'|----|', $obj2->hash;
+if($obj === $obj2){
+    echo '是同一个对象<br />';
+}else{
+    echo '不是同一个对象<br />'; 
+}
+
+echo '<h3>Program Ring is ok! </h3>';
+
+
+?>
