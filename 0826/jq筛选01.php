1e3ff830f7845d44172e8c74f154107a46f034c3
GongBiao <developbiao@gmail.com>
2015-08-26 21:15:27 +0800

    简单的练习了一下jquery筛选

---
+++ 0826/jq筛选01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
+<title>index</title>
+<script src="./jquery.js"></script>
+<script type="text/javascript">
+	$(document).ready(function (){
+		$("h3").last().css("background", "purple");
+	});
+</script>
+</head>
+<body>
+	<h3>aaaaaa</h3>
+	<h3>aaaaaa</h3>
+	<h3>aaaaaa</h3>
+	<h3>aaaaaa</h3>
+	<h3>aaaaaa</h3>
+</body>
+</html>
