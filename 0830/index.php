bbebba18abf08b48d3f981077940cbd6615381ab
GongBiao <developbiao@gmail.com>
2015-08-30 20:09:24 +0800

    看了一下午的公司代码,看懂了师傅写的对象初始化过程

---
+++ 0830/index.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type:text/html; charset=utf-8');
+require_once('rb.php');
+R::setup('mysql:host=localhost; dbname=demo', 'root', '123456');
+//$book = R::dispense('book');
+
+//$record = R::findOne('book', 'id=?' , array(3));
+$record = R::findAll('book', 'rating > ?', array(3));
+$list = R::beansToArray($record);
+
+echo '<pre>';
+print_r($list);
+echo '</pre>';
+
+echo '<h3>Programe runing is ok!</h3>';
+?>
