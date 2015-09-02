a126255ba3964f72bd15bf85c9731a07159a9ed7
GongBiao <developbiao@gmail.com>
2015-09-03 00:31:26 +0800

    测试添加了解决方案03

--- 0902/lesson解决方案02.php
+++ 0902/lesson解决方案02.php
@@ -1,19 +1,19 @@
 <?php
 /*
-@describe:从数据库中取出当天的lesson解决方案01
+@Describe:从数据库中取出当天的lesson解决方案01
 */
 require('rb.php');
-r::setup('mysql:host=localhost;dbname=woogi0_1', 'root', '123456');
+R::setup('mysql:host=localhost;dbname=woogi0_1', 'root', '123456');
 
 $list = R::getAll("SELECT lesson, count(*) AS total, DATE_FORMAT(ndate, '%Y-%m-%d') AS day FROM `weanswer` 
-where ndate BETWEEN '2014-06-01' and '2015-06-04' and type='question' 
+where ndate BETWEEN '2015-06-01' and '2015-06-04' and type='question' 
 AND lesson in('1', '2', '3', '4', '5', '6', '7', '8', '9')
 GROUP BY DATE_FORMAT(ndate, '%Y-%m-%d'), lesson 
 order by ndate desc;
 ");
 
 
- function date_range($first, $last, $step = '+1 day', $output_format = 'd/m/y' )
+ function date_range($first, $last, $step = '+1 day', $output_format = 'd/m/Y' )
     {
 
         $dates = array();
@@ -29,7 +29,7 @@ order by ndate desc;
         return $dates;
     }
 
-$date_start = '2014-06-01';
+$date_start = '2015-06-01';
 $date_end = '2015-06-04';
 $dates = date_range($date_start, $date_end, '+1 day', 'Y-m-d');
 
@@ -41,17 +41,12 @@ for($i=1; $i<=9; $i++){
 	$key = sprintf("lesson%s", $i); //lesson
 	foreach($dates as $day){
 		$result[$key][$day] = 0; //数据初始化为0
-		if(!empty($list)){
 		foreach($list as $ele){
 			if($ele['day'] == $day && $ele['lesson'] == $i){
 				$result[$key][$day] = $ele['total'];
 			}
 		}
-		}else{
-			$return[$key][$day] = 0;   //day 可以在项目去掉
-		}
 	}
-
 }
 
 
@@ -63,14 +58,6 @@ echo '<pre>';
 print_r($result);
 echo '</pre>';
 
-echo '<hr>';
-
-/*
-echo '<pre>';
-print_r($list);
-echo '</pre>';
-*/
-
-echo '<h3>program runing is ok!</h3>';
+echo '<h3>Program runing is ok!</h3>';
 
 ?>
