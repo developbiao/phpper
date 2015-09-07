680b807fc4851cfb6239523dc062108af996f314
GongBiao <developbiao@gmail.com>
2015-09-08 06:00:34 +0800

    php常用函数练习到第10个

--- 0906/index.php
+++ 0906/index.php
@@ -4,51 +4,22 @@
 *@Author:GongBiao
 *@Date:2015/09/06
 */
+define ( "MAXSIZE" ,  100 );
 
-//从父作用域继承变量
-$message = 'hello';
-
-//没有 use
-/*
-$example = function (){
-	var_dump($message); //Notice: Undefined variable: message in E:\Code\phpper\0906\index.php on line 13
-NULL 
-};
-
-echo $example();
-*/
-
-//继承$message
-$example = function () use($message){
-	var_dump($message);
-};
-
-echo $example(); //string(5) "hello" 
-
-// is defined, not when called
-$message = 'world';
-echo $example();
-
-//Reset message
-$message = 'hello';
-
-//Inherit by-reference 引用
-$example = function () use(&$message){
-	var_dump($message);
-};
-
-echo $example(); //hello
-
-$message = 'world'; //world
-echo $example();
-
-//闭包可接受常规参数
-$example = function ($arg) use($message){
-	var_dump($arg. '---'. $message);
-};
-
-$example('GongBiao'); // string(16) "GongBiao---world" 
+echo  MAXSIZE ;
+echo  constant ( "MAXSIZE" );  // same thing as the previous line
 
 
+ interface  bar  {
+    const  test  =  'foobar!' ;
+}
 
-?>
+class  foo  {
+    const  test  =  'newspaper!' ;
+}
+
+ $const  =  'test' ;
+
+ var_dump ( constant ( 'bar::' .  $const ));  // string(7) "foobar!"
+ var_dump ( constant ( 'foo::' .  $const ));  // string(x) "newspaper!"
+ ?>
