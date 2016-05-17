bb0e4c604c07503267fe5241ed4048af6253e276
developbiao <developbiao@gmail.com>
2016-05-17 08:14:30 +0800

    HTML5新from标签特性

---
+++ 0517/action02.php
@@ -0,0 +1,4 @@
+<?php
+header('Content-Type:text/html; charset=utf-8');
+echo '<h3>这是第二个页面</h3>';
+?>
---
+++ 0517/action03.php
@@ -0,0 +1,4 @@
+<?php
+header('Content-Type:text/html; charset=utf-8');
+echo '<h3>这是第三个页面</h3>';
+?>
