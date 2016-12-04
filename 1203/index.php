fc9e81d839a4343a1c4934ff10bf57e6b92ebd12
gongbiao <developbiao@gmail.com>
2016-12-04 22:48:13 +0800

    Learn Bootstrp grid and php ref-doc

---
+++ 1203/index.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(E_ALL);
+ini_set('html_errors', 1);
+ini_set('docref_root', "https://secure.php.net/manual/zh/");
+echo "<h3> well test html errors</h3>";
+include("a2.php"); //E_WARNING
+?>
